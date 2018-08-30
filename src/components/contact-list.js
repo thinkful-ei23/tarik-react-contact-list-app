f54e56830216367389c5fbff60de0a62d8a20615
Tarik Desire <tprdesire@gmail.com>
2018-08-30 11:32:44 -0400

    Added delete, working on editing functionality

--- src/components/contact-list.js
+++ src/components/contact-list.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 export default function ContactList(props) {
+  // const dummyContacts = props.contacts;
+
+  // if (props.editMode) {
+  //   dummyContacts.map
+  // }
   const dummyContacts = props.contacts.map((contact, index) => 
     <li className="contact" aria-label="contact" key={index}>
       <span>Name: {contact.name}</span>
@@ -8,6 +13,9 @@ export default function ContactList(props) {
       <span>Phone-number: {contact.number}</span>
       <br />
       <span>Email: {contact.email}</span>
+      <br />
+      <button type="button" className="EditButton" value="edit" name="edit" data-owner={contact.name} onClick={props.onClick}>Edit</button>
+      <button type="button" className="DeleteButton" value="delete" name="delete" data-owner={contact.name} onClick={props.onClick}>Delete</button>
     </li>
     );
 
--- src/components/user-interface.js
+++ src/components/user-interface.js
@@ -10,17 +10,20 @@ export default class UserInterface extends React.Component {
         {
             "name": "John Doe",
             "number": "555-555-5555",
-            "email": "jdoe@thinkful.com"
+            "email": "jdoe@thinkful.com",
+            "editMode": false
         },
         {
             "name": "Jane Doe",
             "number": "777-777-7777",
-            "email": "jane.doe@thinkful.com"
+            "email": "jane.doe@thinkful.com",
+            "editMode": false
         },
         {
             "name": "Joe Shmoe",
             "number": "999-999-9999",
-            "email": "jshmoe@thinkful.com"
+            "email": "jshmoe@thinkful.com",
+            "editMode": false
         }
       ],
       name: '',
@@ -41,19 +44,38 @@ export default class UserInterface extends React.Component {
     const newContact = {
       name: this.state.name,
       number: this.state.number,
-      email: this.state.email
-    };
+      email: this.state.email,
+      editMode: false
+    }
 
     this.setState({
       contacts: [...this.state.contacts, newContact]
     });
+  }
+
+  handleButtonClick(event) {
+    if(event.target.name === "delete") {
+      console.log('handeButtonClick ran for a deletion!');
+      console.log(event.target.dataset.owner);
+      const contacts = this.state.contacts.filter(contact => contact.name!== event.target.dataset.owner);
+      this.setState({
+        contacts
+      });
+    } else {
+      console.log('handleButtonClick ran for an edit');
+      const contacts = this.state.contacts.map(contact => contact.name=== event.target.dataset.owner ? {...contact, editMode:!contact.editMode} : contact);
+      console.log(contacts);
+      this.setState({
+        contacts
+      })
+    }
   }
   
   render() {
     return (
       <div className="UserInterface">
         <CreateContact onChange={(e) => {this.onChange(e)}} onSubmit={(e) => {this.onSubmit(e)}} />
-        <ContactList contacts={this.state.contacts} />
+        <ContactList contacts={this.state.contacts} onClick={(e) => this.handleButtonClick(e)} />
       </div>
     )
   }
